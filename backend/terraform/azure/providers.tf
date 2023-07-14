331cbe816aac212840816dfbe563316c7a4ce282
ABISHEK A T <43531987+abishekat@users.noreply.github.com>
2023-07-14 18:35:49 -0400

    Update providers.tf

--- backend/terraform/azure/providers.tf
+++ backend/terraform/azure/providers.tf
@@ -3,10 +3,23 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
     }
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "s3" {
+    bucket         = "bucket-tfstate-test"
+    key            = "state-file/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
   }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "d143290a-4d5b-41d1-9223-a02a18155031"
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
