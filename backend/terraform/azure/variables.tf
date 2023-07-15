ee5ed07337f11654579636563048a76b19976962
ABISHEK A T <43531987+abishekat@users.noreply.github.com>
2023-07-15 10:18:53 -0400

    Create variables.tf

---
+++ backend/terraform/azure/variables.tf
@@ -0,0 +1,19 @@
+variable "azure_client_id" {
+  description = "Username for the Docker container"
+}
+
+variable "azure_password" {
+  description = "Password for the Docker container"
+}
+
+variable "azure_tenant_id" {
+  description = "Password for the Docker container"
+}
+
+variable "registry_password" {
+  description = "Password for the xaicontainerregistry"
+}
+
+variable "subscription_id" {
+  description = "Azure subscription id"
+}
