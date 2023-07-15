d68681da27377baf37debe4121b9b8494a0fbc8d
ABISHEK A T <43531987+abishekat@users.noreply.github.com>
2023-07-15 19:32:57 -0400

    Update providers.tf

--- backend/terraform/azure/providers.tf
+++ backend/terraform/azure/providers.tf
@@ -1,7 +1,20 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_password
+  tenant_id       = var.azure_tenant_id
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
     aws = {
       source = "hashicorp/aws"
@@ -15,14 +28,9 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_password
-  tenant_id       = var.azure_tenant_id
-}
-
-provider "aws" {
-  region = "us-east-1"
+resource "azurerm_virtual_network" "xai-vn" {
+  name                = "xai-vn"
+  resource_group_name = "aci-xai-rg"
+  location            = "East US"
+  address_space       = ["10.0.0.0/16"]
 }
